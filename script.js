305ac48cc31e803eac228f3cf04b950818ef140a
Vicky Dhurwey <dhurweyvicky317@gmail.com>
2026-07-30 06:54:26 +0530

    Enhance loading screen with progress updates

--- script.js
+++ script.js
@@ -1,14 +1,42 @@
-document.addEventListener("DOMContentLoaded", () => {
+let progress = document.getElementById("progress");
+let percent = document.getElementById("percent");
+let status = document.getElementById("status");
 
-    const title = document.querySelector("h1");
+let boot = document.getElementById("boot-screen");
+let dashboard = document.getElementById("dashboard");
 
-    title.style.opacity = "0";
-    title.style.transform = "scale(0.7)";
+let value = 0;
 
-    setTimeout(() => {
-        title.style.transition = "all 1.5s ease";
-        title.style.opacity = "1";
-        title.style.transform = "scale(1)";
-    }, 300);
+let messages = [
+    "Loading Core...",
+    "Loading AI Engine...",
+    "Connecting Network...",
+    "Checking Security...",
+    "Starting Voice Engine...",
+    "Launching V!KKU V3..."
+];
 
-});
+let index = 0;
+
+let loading = setInterval(() => {
+
+    value++;
+
+    progress.style.width = value + "%";
+    percent.innerHTML = value + "%";
+
+    if (value % 20 === 0 && index < messages.length) {
+        status.innerHTML = messages[index];
+        index++;
+    }
+
+    if (value >= 100) {
+
+        clearInterval(loading);
+
+        boot.style.display = "none";
+        dashboard.style.display = "flex";
+
+    }
+
+}, 50);
